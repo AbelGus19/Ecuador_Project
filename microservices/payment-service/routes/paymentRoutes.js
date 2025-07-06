57dffbe9e680c4b20c946ae957ed7dd0b2b1e982
AbelGus <clavitomartillo@gmail.com>
2025-07-06 16:30:11 -0500

    Update Files

--- microservices/payment-service/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../controllers/paymentController');
-const authenticate = require('../middleware/auth');
-
-router.post('/', authenticate, paymentController.createPayment);
-router.get('/my', authenticate, paymentController.getMyPayments);
-
-module.exports = router;
